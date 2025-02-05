1deef25e660f26c6c31f59612c75fc5e105eba52
DmitriiKovrigin <dimals18@icloud.com>
2025-02-05 12:32:06 +0300

    Initial commit

---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by Dima Kovrigin on 05.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
